7e962f944ce273e0d0da354e76291a9320c604e9
Kengah Ireneaus <4jrkit1@gmail.com>
2026-09-17 12:30:48 +0100

    feat(clipmanager): add toggle is_pinned method

--- src/clipk/operators/clipmanager.py
+++ src/clipk/operators/clipmanager.py
@@ -14,6 +14,8 @@ class Clip:
     """ clip class specifies behaviors and attribute of a clipped item """
     ALLOWED_TYPES = ("text", "img", "video")
 
+    MAX_CLIPS = 10
+
     def __init__(self, clip_data: str, clip_type: str = "text",
                 clip_path: Path | str | None = None,
                 date_clipped: datetime | None = None,
@@ -28,17 +30,25 @@ class Clip:
     def is_type(self):
         return self.clip_type.strip().lower() in self.ALLOWED_TYPES
 
-    def pin_clip(self):
-        self.is_pinned = True
-
-
+    def toggle_pin(self):
+        if not self.is_pinned:
+            self.is_pinned = True
+        else:
+            self.is_pinned = False
+        return self.is_pinned
+    
     def get_unique_id(self) -> str:
-        id_bytes = self.clip_data.encode()
-        id_hash = str(int(hashlib.sha256(id_bytes).hexdigest(), 16))  # hash and convert the hash string to into str(numbers)
-
+        """generate unique id from data"""
         TYPE_MAP = {"text": "t", "img":"i", "video":"v"}
+        try:
+            id_bytes = self.clip_data.encode()
+            id_hash = str(int(hashlib.sha256(id_bytes).hexdigest(), 16))  # hash and convert the hash string to into str(numbers)
+        except UnicodeEncodeError:
+            print("[error] failed to encode", file=sys.stderr) # will modify later to a log file
+            return f"{self.clip_data[:3]}F"
 
         return f"{id_hash[:3]}{TYPE_MAP[self.clip_type]}{id_hash[1]}"
+
 
 
 
