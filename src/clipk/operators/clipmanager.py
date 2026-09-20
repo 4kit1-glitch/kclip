005d059ae03a1824f56472b654772928528fc7ff
Kengah Ireneaus <4jrkit1@gmail.com>
2026-09-20 14:06:33 +0100

    fix: cleaned format with ruff auto formatter

--- src/clipk/operators/clipmanager.py
+++ src/clipk/operators/clipmanager.py
@@ -1,11 +1,10 @@
 """
-    Manager module for kclip
-        - contains clip class
-        - contains specific class type subclasses
-        - contains other general functions
-    
-"""
+Manager module for kclip
+    - contains clip class
+    - contains specific class type subclasses
+    - contains other general functions
 
+"""
 
 import sys
 import hashlib
@@ -14,20 +13,26 @@ from typing import ClassVar
 from pathlib import Path
 
 
-
 PROGRAM_PATH = __file__
 
+
 class Clip:
-    """ clip class specifies behaviors and attribute of a clipped item """
+    """clip class specifies behaviors and attribute of a clipped item"""
+
     ALLOWED_TYPES = ("text", "img", "video", "audio")
     MAX_CLIPS: ClassVar[int] = 10
     CLIP_COUNT: ClassVar[int] = 0
-    all_clips : ClassVar[list[str]] = []
-    def __init__(self, clip_data: str, clip_type: str = "text",
-                clip_path: Path | str | None = None,
-                date_clipped: datetime | None = None,
-                is_pinned: bool = False) -> None:
-        
+    all_clips: ClassVar[list[str]] = []
+
+    def __init__(
+        self,
+        clip_data: str,
+        clip_type: str = "text",
+        clip_path: Path | str | None = None,
+        date_clipped: datetime | None = None,
+        is_pinned: bool = False,
+    ) -> None:
+
         self.clip_type = clip_type
         self.clip_path = clip_path
         self.date_clipped = date_clipped
@@ -41,27 +46,30 @@ class Clip:
         self.is_pinned = not self.is_pinned
         return self.is_pinned
 
-    
     def get_unique_id(self) -> str:
         """generate unique id from data"""
-        TYPE_MAP = {"text": "t", "img":"i", "video":"v", "audio": "a"}
+        TYPE_MAP = {"text": "t", "img": "i", "video": "v", "audio": "a"}
         try:
             id_bytes = self.clip_data.encode()
-            id_hash = str(int(hashlib.sha256(id_bytes).hexdigest(), 16))  # hash and convert the hash string to into str(numbers)
+            id_hash = str(
+                int(hashlib.sha256(id_bytes).hexdigest(), 16)
+            )  # hash and convert the hash string to into str(numbers)
         except UnicodeEncodeError:
-            print("[error] failed to encode", file=sys.stderr) # will modify later to a log file
+            print(
+                "[error] failed to encode", file=sys.stderr
+            )  # will modify later to a log file
             return f"{self.clip_data[:3]}F"
 
         return f"{id_hash[:3]}{TYPE_MAP[self.clip_type]}{id_hash[1]}"
 
     @classmethod
     def add_clip(cls, clip_obj: "Clip") -> str:
-        """ add a new clip return unique id of clip via unique_id"""
+        """add a new clip return unique id of clip via unique_id"""
         unique_id = clip_obj.get_unique_id()
 
-        if unique_id in cls.all_clips: # fail sfe to catch dublicate cliped items 
+        if unique_id in cls.all_clips:  # fail sfe to catch dublicate cliped items
             return unique_id
-        
+
         if not isinstance(clip_obj, Clip):
             raise TypeError
 
@@ -74,10 +82,10 @@ class Clip:
             cls.CLIP_COUNT += 1
 
         return unique_id
-    
+
     @classmethod
     def remove_clip(cls, unique_id: str) -> str:
-        """ remove a given clip with a unique id"""
+        """remove a given clip with a unique id"""
         try:
             cls.all_clips.remove(unique_id)
             cls.CLIP_COUNT -= 1
@@ -85,15 +93,18 @@ class Clip:
             print(f"Failed to remove clip ID{unique_id}", file=sys.stderr)
         return unique_id
 
-        
-
 
 class TextClip(Clip):
-    """ specific class to text clips """
-    def __init__(self, clip_data: str, clip_type: str = "text",
-                 clip_path: Path | str | None = None, 
-                 date_clipped: datetime | None = None, 
-                 is_pinned: bool = False) -> None:
+    """specific class to text clips"""
+
+    def __init__(
+        self,
+        clip_data: str,
+        clip_type: str = "text",
+        clip_path: Path | str | None = None,
+        date_clipped: datetime | None = None,
+        is_pinned: bool = False,
+    ) -> None:
         super().__init__(clip_data, clip_type, clip_path, date_clipped, is_pinned)
 
         self.clip_name = self.clip_data[:15]
@@ -102,10 +113,14 @@ class TextClip(Clip):
 class ImageClip(Clip):
     """specific class to imageclips"""
 
-    def __init__(self, clip_data: str, clip_type: str = "img", 
-                 clip_path: Path | str | None = None, 
-                 date_clipped: datetime | None = None, 
-                 is_pinned: bool = False) -> None:
+    def __init__(
+        self,
+        clip_data: str,
+        clip_type: str = "img",
+        clip_path: Path | str | None = None,
+        date_clipped: datetime | None = None,
+        is_pinned: bool = False,
+    ) -> None:
         super().__init__(clip_data, clip_type, clip_path, date_clipped, is_pinned)
 
         self.clip_name = f"[IMG]{self.clip_path}"
@@ -113,20 +128,31 @@ class ImageClip(Clip):
 
 class AudioClip(Clip):
     """specific class to audio clips"""
-    def __init__(self, clip_data: str, clip_type: str = "audio", 
-                 clip_path: Path | str | None = None, 
-                 date_clipped: datetime | None = None, 
-                 is_pinned: bool = False) -> None:
+
+    def __init__(
+        self,
+        clip_data: str,
+        clip_type: str = "audio",
+        clip_path: Path | str | None = None,
+        date_clipped: datetime | None = None,
+        is_pinned: bool = False,
+    ) -> None:
         super().__init__(clip_data, clip_type, clip_path, date_clipped, is_pinned)
 
         self.clip_name = f"[AUDIO]{self.clip_path}"
 
+
 class VideoClip(Clip):
     """specific class to video clips"""
-    def __init__(self, clip_data: str, clip_type: str = "video", 
-                 clip_path: Path | str | None = None, 
-                 date_clipped: datetime | None = None, 
-                 is_pinned: bool = False) -> None:
-        super().__init__(clip_data, clip_type,clip_path, date_clipped, is_pinned)
+
+    def __init__(
+        self,
+        clip_data: str,
+        clip_type: str = "video",
+        clip_path: Path | str | None = None,
+        date_clipped: datetime | None = None,
+        is_pinned: bool = False,
+    ) -> None:
+        super().__init__(clip_data, clip_type, clip_path, date_clipped, is_pinned)
 
         self.clip_name = f"[VIDEO]{self.clip_path}"
--- src/clipk/operators/storeengine.py
+++ src/clipk/operators/storeengine.py
@@ -1,14 +1,13 @@
 """
-    Storage engine module for kclip
-    contains functions to:
-        - manipulate database
-        - create nessesary dirs
-        - add remove modify a clip
-        - open and close connections
-        - initialize connection
+Storage engine module for kclip
+contains functions to:
+    - manipulate database
+    - create nessesary dirs
+    - add remove modify a clip
+    - open and close connections
+    - initialize connection
 
 """
-
 
 import os
 import sys
@@ -18,7 +17,6 @@ from pathlib import Path
 from clipmanager import Clip, TextClip, AudioClip, ImageClip, VideoClip
 
 
-
 APP_NAME = "kclip"
 DATABASE_NAME = "kclip.db"
 
@@ -26,49 +24,52 @@ _conn = None
 _initialized = False
 
 
-        
-
 def get_data_dir() -> Path:
-    """ returns linux standard data dir"""
+    """returns linux standard data dir"""
     xdg_data_home = os.environ.get("XDG_DATA_HOME")
     if xdg_data_home:
         data_dir = Path(xdg_data_home) / APP_NAME / "data"
     else:
-        data_dir = Path.home() / ".local"/ "share" / APP_NAME / "data"
+        data_dir = Path.home() / ".local" / "share" / APP_NAME / "data"
     return data_dir
 
+
 def get_db_dir() -> Path:
-    """ returns the path of database"""
+    """returns the path of database"""
     return get_data_dir() / "db"
+
 
 def get_db_path() -> Path:
     return get_db_dir() / DATABASE_NAME
+
 
 def close_db() -> None:
     global _conn
     if _conn is not None:
         _conn.close()
         _conn = None
-    
+
+
 def get_connection():
     global _conn
 
     if _conn is None:
         _conn = sqlite3.connect(get_db_path())
-        _conn.execute("PRAGMA journal_mode=WAL") # this ensures the database doesnt crash on simultanuous reads
+        _conn.execute(
+            "PRAGMA journal_mode=WAL"
+        )  # this ensures the database doesnt crash on simultanuous reads
         _conn.row_factory = sqlite3.Row
         atexit.register(close_db)
-       
+
     return _conn
 
-        
 
 def create_dir(path: Path):
     path.mkdir(parents=True, exist_ok=True)
-    
+
 
 def create_db() -> None:
-    """ creates the kclip database """
+    """creates the kclip database"""
 
     conn = get_connection()
 
@@ -83,8 +84,9 @@ def create_db() -> None:
             )
     """)
 
+
 def delete_db() -> None:
-    """ deletes kclip database """
+    """deletes kclip database"""
     db_path = get_db_path()
     try:
         close_db()
@@ -96,30 +98,38 @@ def delete_db() -> None:
         db_path.with_suffix(".db-shm").unlink(missing_ok=True)
 
 
-
-def add_record(unique_name: str, clip_type: str, is_pinned: bool ,clip_path: Path | str | None = None) -> None:
-    """ adds a record to database """
+def add_record(
+    unique_name: str,
+    clip_type: str,
+    is_pinned: bool,
+    clip_path: Path | str | None = None,
+) -> None:
+    """adds a record to database"""
     pinstatus = 1 if is_pinned else 0
     path_value = str(clip_path) if clip_path is not None else None
 
     conn = get_connection()
 
     conn.execute(
-        "INSERT INTO clips (uniqueName, clipType, clipPath, isPinned) VALUES (?, ?, ?, ?)", 
-        (unique_name, clip_type, path_value, pinstatus)
+        "INSERT INTO clips (uniqueName, clipType, clipPath, isPinned) VALUES (?, ?, ?, ?)",
+        (unique_name, clip_type, path_value, pinstatus),
     )
     conn.commit()
 
 
-def read_record(clip_id: int| None = None, unique_name: str | None = None) -> dict | None:
-    """ reads a specific record from database provided with either the clip_id or
-        unique_name
+def read_record(
+    clip_id: int | None = None, unique_name: str | None = None
+) -> dict | None:
+    """reads a specific record from database provided with either the clip_id or
+    unique_name
     """
     conn = get_connection()
     if clip_id is not None:
         result = conn.execute(" SELECT * FROM clips WHERE clipID = ?", (clip_id,))
     elif unique_name is not None:
-        result = conn.execute(" SELECT * FROM clips WHERE uniqueName = ?", (unique_name,))
+        result = conn.execute(
+            " SELECT * FROM clips WHERE uniqueName = ?", (unique_name,)
+        )
     else:
         return None
 
@@ -131,42 +141,32 @@ def read_record(clip_id: int| None = None, unique_name: str | None = None) -> di
     return dict(record)
 
 
-            
-        
 def delete_record(clip_id: int) -> dict | None:
-    """ removes a record form database , returns the deleted record as dict """
+    """removes a record form database , returns the deleted record as dict"""
     conn = get_connection()
 
     record = read_record(clip_id)
     if record is None:
         return None
-    
-    conn.execute(
-        "DELETE FROM clips where clipID = ?", (clip_id,)
-    )
+
+    conn.execute("DELETE FROM clips where clipID = ?", (clip_id,))
     conn.commit()
 
     return record
 
 
-
 def add_clip_to_db(clip: Clip | TextClip | AudioClip | ImageClip | VideoClip) -> None:
-    """ adds a clip object to the database"""
-    add_record(
-        clip.get_unique_id(), 
-        clip.clip_type, 
-        clip.is_pinned, 
-        clip.clip_path
-    )
+    """adds a clip object to the database"""
+    add_record(clip.get_unique_id(), clip.clip_type, clip.is_pinned, clip.clip_path)
 
 
 def remove_clip_from_db(clip_id: int):
-    """ removes a clip object """
+    """removes a clip object"""
     delete_record(clip_id)
 
 
 def init() -> bool:
-    """ runs proper proceedings to setup database"""
+    """runs proper proceedings to setup database"""
     global _initialized
     if _initialized:
         return True
@@ -181,4 +181,3 @@ def init() -> bool:
         print(f"[Error] failed to initialize : {e}", file=sys.stderr)
         _initialized = False
         return False
-    
